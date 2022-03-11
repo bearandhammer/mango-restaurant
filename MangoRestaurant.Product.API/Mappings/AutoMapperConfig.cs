5686b47fcabfdec1a9a4c696bdcefa0af7683a0e
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-11 08:52:43 +0000

    Add simple mapper for Product/ProductDto.

---
+++ MangoRestaurant.Product.API/Mappings/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MangoRestaurant.Product.API.Models.Dtos;
+
+namespace MangoRestaurant.Product.API.Mappings
+{
+    public static class AutoMapperConfig
+    {
+        // TODO - switch out for configurations
+        public static MapperConfiguration RegisterMappings()
+        {
+            MapperConfiguration typeMaps = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Models.Entities.Product, ProductDto>().ReverseMap();
+            });
+
+            return typeMaps;
+        }
+    }
+}
