c16767b4a65c5cb792234e321a82b61c16f15e11
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-12 12:45:11 +0000

    Further operations added to the ProductController.

--- MangoRestaurant.Product.API/Controllers/ProductController.cs
+++ MangoRestaurant.Product.API/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MangoRestaurant.Product.API.Controllers
 {
+    // TODO - Tidy implementation, would prefer to split into services (clean this down)
     [Route("api/products")]
     public class ProductController : ControllerBase
     {
@@ -12,6 +13,29 @@ namespace MangoRestaurant.Product.API.Controllers
         public ProductController(IProductRepository productRepo)
         {
             productRepository = productRepo;
+        }
+
+        [HttpDelete]
+        public async Task<ResponseDto<bool>> Delete(int id)
+        {
+            // TODO: ICollection?
+            ResponseDto<bool> response = new ResponseDto<bool>();
+
+            try
+            {
+                bool isOperationSuccessful = await productRepository.DeleteProduct(id);
+                response.Result = isOperationSuccessful;
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log
+                response.IsSuccess = false;
+
+                // TODO: To improve (example from course)
+                response.ErrorMessages = new List<string> { ex.Message };
+            }
+
+            return response;
         }
 
         [HttpGet]
@@ -37,7 +61,6 @@ namespace MangoRestaurant.Product.API.Controllers
             return response;
         }
 
-
         [HttpGet]
         [Route("{id}")]
         public async Task<ResponseDto<ProductDto>> Get(int id)
@@ -49,6 +72,53 @@ namespace MangoRestaurant.Product.API.Controllers
             {
                 ProductDto productResult = await productRepository.GetProductById(id);
                 response.Result = productResult;
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log
+                response.IsSuccess = false;
+
+                // TODO: To improve (example from course)
+                response.ErrorMessages = new List<string> { ex.Message };
+            }
+
+            return response;
+        }
+
+        // TODO: Course Upserts (to revisit is the Post/Put mechanic, not sure I like it)
+        [HttpPost]
+        public async Task<ResponseDto<ProductDto>> Post([FromBody] ProductDto productDto)
+        {
+            // TODO: ICollection?
+            ResponseDto<ProductDto> response = new ResponseDto<ProductDto>();
+
+            try
+            {
+                ProductDto productUpsertResult = await productRepository.UpsertProduct(productDto);
+                response.Result = productUpsertResult;
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log
+                response.IsSuccess = false;
+
+                // TODO: To improve (example from course)
+                response.ErrorMessages = new List<string> { ex.Message };
+            }
+
+            return response;
+        }
+
+        [HttpPut]
+        public async Task<ResponseDto<ProductDto>> Put([FromBody] ProductDto productDto)
+        {
+            // TODO: ICollection?
+            ResponseDto<ProductDto> response = new ResponseDto<ProductDto>();
+
+            try
+            {
+                ProductDto productUpsertResult = await productRepository.UpsertProduct(productDto);
+                response.Result = productUpsertResult;
             }
             catch (Exception ex)
             {
