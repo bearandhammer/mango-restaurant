b4b00deeebfb5d201c7cb5b24289b0ac57af08e9
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-12 12:06:04 +0000

    Get operations stubbed.

--- MangoRestaurant.Product.API/Repositories/ProductRepository.cs
+++ MangoRestaurant.Product.API/Repositories/ProductRepository.cs
@@ -1,23 +1,40 @@
-﻿using MangoRestaurant.Product.API.Models.Dtos;
+﻿using AutoMapper;
+using MangoRestaurant.Product.API.Context;
+using MangoRestaurant.Product.API.Models.Dtos;
 using MangoRestaurant.Product.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace MangoRestaurant.Product.API.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly ProductDbContext dbContext;
+
+        private readonly IMapper mapper;
+
+        public ProductRepository(ProductDbContext dbContextType, IMapper mapperType)
+        {
+            dbContext = dbContextType;
+            mapper = mapperType;
+        }
+
         public Task<bool> DeleteProduct(int productId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ProductDto>> GetAllProducts()
+        public async Task<IEnumerable<ProductDto>> GetAllProducts()
         {
-            throw new NotImplementedException();
+            IEnumerable<Models.Entities.Product> allProducts = await dbContext.Products.ToListAsync();
+
+            return mapper.Map<IEnumerable<ProductDto>>(allProducts);
         }
 
-        public Task<ProductDto> GetProductById(int productId)
+        public async Task<ProductDto> GetProductById(int productId)
         {
-            throw new NotImplementedException();
+            Models.Entities.Product discoveredProduct = await dbContext.Products.FirstOrDefaultAsync(product => product.Id == productId);
+
+            return mapper.Map<ProductDto>(discoveredProduct);
         }
 
         public Task<ProductDto> UpsertProduct(ProductDto productDto)
