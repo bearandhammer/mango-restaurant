c45af3a9fc7c01da74b4f014b762dcb76d793aaf
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-12 13:32:21 +0000

    Copying over Dto files.
    This is horrible (following course for now, will have to adjust).

---
+++ MangoRestaurant.Web/Models/Dtos/ResponseDto.cs
@@ -0,0 +1,29 @@
+﻿namespace MangoRestaurant.Web.Models.Dtos
+{
+    /// <summary>
+    /// Represents a response wrapper for all results from returned for this API.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    public class ResponseDto<TResult>
+    {
+        /// <summary>
+        /// Gets or sets the Display Message.
+        /// </summary>
+        public string DisplayMessage { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the Error Messages tied to this response.
+        /// </summary>
+        public List<string>? ErrorMessages { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this operation was successful.
+        /// </summary>
+        public bool IsSuccess { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the result of this operation.
+        /// </summary>
+        public TResult? Result { get; set; }
+    }
+}
