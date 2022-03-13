9877b2d4f00bc2d86649156f2f6ad257238eeb55
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-13 12:07:54 +0000

    Added MangoRestaurant.Services.Identity project.
    Identity Server.

---
+++ MangoRestaurant.Services.Identity/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace mango_restaurant.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
