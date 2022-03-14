de6e0852d27affe0df056603d0b699434b184cce
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-14 08:52:27 +0000

    Additional migration for Application User (First/Last name additions).

--- MangoRestaurant.Services.Identity/Migrations/20220314084654_AddFirstLastNameToApplicationUser.Designer.cs
+++ MangoRestaurant.Services.Identity/Migrations/20220314084654_AddFirstLastNameToApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using MangoRestaurant.Services.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MangoRestaurant.Services.Identity.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220314084654_AddFirstLastNameToApplicationUser")]
+    partial class AddFirstLastNameToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
