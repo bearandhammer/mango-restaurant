13e89f5e53434456bde88c7097622e142106756c
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-14 19:22:47 +0000

    Complete test data seeding.

--- MangoRestaurant.Services.Identity/Initializer/DbInitializer.cs
+++ MangoRestaurant.Services.Identity/Initializer/DbInitializer.cs
@@ -1,8 +1,10 @@
-﻿using MangoRestaurant.Services.Identity.Data;
+﻿using IdentityModel;
+using MangoRestaurant.Services.Identity.Data;
 using MangoRestaurant.Services.Identity.Initializer.Interfaces;
 using MangoRestaurant.Services.Identity.Models;
 using MangoRestaurant.Services.Identity.Utilities;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace MangoRestaurant.Services.Identity.Initializer
 {
@@ -31,27 +33,66 @@ namespace MangoRestaurant.Services.Identity.Initializer
             }
 
             IdentityRole customerRole = roleManager.FindByNameAsync(IdentityHelper.CustomerRole).GetAwaiter().GetResult();
-            if (adminRole == null)
+            if (customerRole == null)
             {
                 roleManager.CreateAsync(new IdentityRole(IdentityHelper.CustomerRole));
             }
 
             // Configure Users
-            ApplicationUser adminUser = userManager.FindByNameAsync("admin1@gmail.com").GetAwaiter().GetResult();
+            string userEmail = "admin1@gmail.com";
+
+            ApplicationUser adminUser = userManager.FindByNameAsync(userEmail).GetAwaiter().GetResult();
             if (adminUser == null)
             {
                 adminUser = new ApplicationUser()
                 {
-                    UserName = "admin1@gmail.com",
-                    Email = "admin1@gmail.com",
+                    UserName = userEmail,
+                    Email = userEmail,
                     EmailConfirmed = true,
                     PhoneNumber = "01508574837",
                     FirstName = "Ben",
                     LastName = "Admin"
                 };
 
-                userManager.CreateAsync(adminUser, "Admin123").GetAwaiter().GetResult();
+                userManager.CreateAsync(adminUser, "Admin123*").GetAwaiter().GetResult();
                 userManager.AddToRoleAsync(adminUser, IdentityHelper.AdminRole).GetAwaiter().GetResult();
+                userManager.AddClaimsAsync(adminUser, new Claim[]
+                {
+                    new Claim(JwtClaimTypes.Name, adminUser.FullName),
+                    new Claim(JwtClaimTypes.GivenName, adminUser.FirstName ?? string.Empty),
+                    new Claim(JwtClaimTypes.FamilyName, adminUser.LastName ?? string.Empty),
+                    new Claim(JwtClaimTypes.Role, IdentityHelper.AdminRole)
+                })
+                .GetAwaiter()
+                .GetResult();
+            }
+
+            userEmail = "customer1@gmail.com";
+
+            ApplicationUser customerUser = userManager.FindByNameAsync(userEmail).GetAwaiter().GetResult();
+            if (customerUser == null)
+            {
+                customerUser = new ApplicationUser()
+                {
+                    UserName = userEmail,
+                    Email = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "01508738574",
+                    FirstName = "Ben",
+                    LastName = "Cust"
+                };
+
+                userManager.CreateAsync(customerUser, "Admin123*").GetAwaiter().GetResult();
+                userManager.AddToRoleAsync(customerUser, IdentityHelper.CustomerRole).GetAwaiter().GetResult();
+                userManager.AddClaimsAsync(customerUser, new Claim[]
+                {
+                    new Claim(JwtClaimTypes.Name, customerUser.FullName),
+                    new Claim(JwtClaimTypes.GivenName, customerUser.FirstName ?? string.Empty),
+                    new Claim(JwtClaimTypes.FamilyName, customerUser.LastName ?? string.Empty),
+                    new Claim(JwtClaimTypes.Role, IdentityHelper.CustomerRole)
+                })
+                .GetAwaiter()
+                .GetResult();
             }
         }
     }
