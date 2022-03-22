3d64bd2ebb2287b2216ac7e376c2158fc820a68b
Lewis Grint <lgrint1@yahoo.co.uk>
2022-03-22 08:42:38 +0000

    Add stock register page (ready for content).

---
+++ MangoRestaurant.Services.Identity/Pages/Account/Logout/Registercshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MangoRestaurant.Services.Identity.Pages.Account.Logout
+{
+    public class RegistercshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
