553b70ec3afe0eae2951b283c1ec775c3128c223
Lewis Grint <lgrint1@yahoo.co.uk>
2022-04-19 08:53:53 +0100

    Fleshed out shopping cart mapping config.

--- MongoRestaurant.ShoppingCart.API/Mappings/AutoMapperConfig.cs
+++ MongoRestaurant.ShoppingCart.API/Mappings/AutoMapperConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using MangoRestaurant.ShoppingCart.API.Models.Dtos;
+using MongoRestaurant.ShoppingCart.API.Models.Entity;
 
 namespace MongoRestaurant.ShoppingCart.API.Mappings
 {
@@ -7,8 +9,11 @@ namespace MongoRestaurant.ShoppingCart.API.Mappings
         public static MapperConfiguration RegisterMappings()
         {
             MapperConfiguration typeMaps = new MapperConfiguration(config => 
-            { 
-            
+            {
+                config.CreateMap<Product, ProductDto>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();
             });
 
             return typeMaps;
