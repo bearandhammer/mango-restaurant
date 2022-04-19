de868ad72d20c1848b2040d938ac2bb54f32ad49
Lewis Grint <lgrint1@yahoo.co.uk>
2022-04-19 08:54:09 +0100

    Cart-based repository stub added.

---
+++ MongoRestaurant.ShoppingCart.API/Repositories/CartRepository.cs
@@ -0,0 +1,28 @@
+﻿using MongoRestaurant.ShoppingCart.API.Models.Entity;
+using MongoRestaurant.ShoppingCart.API.Repositories.Interfaces;
+
+namespace MongoRestaurant.ShoppingCart.API.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> UpsertCart(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MongoRestaurant.ShoppingCart.API/Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoRestaurant.ShoppingCart.API.Models.Entity;
+
+namespace MongoRestaurant.ShoppingCart.API.Repositories.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<bool> ClearCart(string userId);
+
+        Task<CartDto> GetCartByUserId(string userId);
+
+        Task<bool> RemoveFromCart(int cartDetailsId);
+
+        Task<CartDto> UpsertCart(CartDto cartDto);
+    }
+}
