701c602bc7876978d6271cc071ddfeb5266be8dd
Lewis Grint <lgrint1@yahoo.co.uk>
2022-05-04 08:40:29 +0100

    Defined ICartService.

---
+++ MangoRestaurant.Web/Services/Interfaces/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using MangoRestaurant.Web.Models.Dtos;
+
+namespace MangoRestaurant.Web.Services.Interfaces
+{
+    public interface ICartService
+    {
+        Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
+
+        Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
+
+        Task<T> RemoveFromCartAsync<T>(int cartDetailsId, string token = null);
+
+        Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
+    }
+}
